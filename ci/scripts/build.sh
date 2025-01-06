904bb6e626b24f26a906a01b578b54b8af0f075f
Matt Chun-Lum <matt@questionable.engineering>
2025-01-06 01:25:54 -0800

    ci: attempt summary

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+cd aoc-tools
+tar -xvf aoc-tools-*-x86_64-unknown-linux-musl.tar.gz
+mv aoc-tools /usr/local/bin/
+cd ../
+aoc-tools --version
+
+cd repo
+
 # This script can pretty much do whatever, but the most basic thing would be
 # ensuring the project's dependencies are installable and that the tests and
 # benchmarks run
@@ -9,4 +17,6 @@ poetry install
 
 # Without filtering any of the marks, this should run the example and real input
 # tests as well as running the benchmarks.
-poetry run pytest tests --benchmark-group-by=name
+poetry run pytest tests -m "bench" --benchmark-group-by=name --benchmark-json=benchmarks.json
+
+aoc-tools python-summary benchmarks.json -l bench-suffixes.json
